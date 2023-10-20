50f967599b6f450ce685072d02c10553f32f94a9
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-10-21 08:17:32 +1100

    installed inquirer, established communication between files, started markdown creation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+const fs = require('fs'); 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -12,9 +13,32 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+fs.writeFile("redMaa.md",
+  `# Title  
+  Welcome to my project  
 
-`;
+  ## Description
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ## Usage
+  ## License
+  ## Contributing
+  ## Tests
+  ## Questions
+  
+  `,
+  (err) => {
+    (err) ? console.log(`NoNo ${err}`) : console.log(`All Good`)});
+
+  return `# ${data.title}`
 }
 
 module.exports = generateMarkdown;
